359eb463174f5ffe3fc42196c1661040d5825b96
Thayane Almeida <thayane.almeida196@gmail.com>
2024-01-07 20:15:34 -0300

    Criação das propriedades, classes Iphone e Nokia

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "1111", modelo: "iPhone 15", imei: "11112222-22", memoria: 516);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "2323", modelo: "G11 Plus", imei: "33334444-43", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
